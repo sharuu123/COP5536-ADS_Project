b92cf0608b77af3c2f8053d7c30ec9aa70910f4a
sharath <sharuu123@gmail.com>
2015-04-15 16:05:44 -0400

    cleaned ssp

--- ssp.cpp
+++ ssp.cpp
@@ -4,6 +4,7 @@
 #include <string>
 #include <cstdlib>
 #include <vector>
+#include <ctime>
 #include "Fibonacci.h"
 #include "dijkstra.h"
 
@@ -11,19 +12,21 @@ using namespace std;
 
 int main(int argc, char *argv[]){
 
-	string filename=argv[1];
-	int src=atoi(argv[2]);
-	int des=atoi(argv[3]);
+    // clock_t start = clock();
+	string filename=argv[1];       // store file name with vertices
+	int src=atoi(argv[2]);         // store source node value as integer
+	int des=atoi(argv[3]);         // store destination node value as integer
 
 	int n,m;
-	unordered_map<int, unordered_map<int,int>> G;
+    // map to store node values and corresponding adjacency lists
+	unordered_map<int, unordered_map<int,int>> G; 
 
 	string line;
 	ifstream myfile(filename);
 	myfile >> n; 		// Vertices
 	myfile >> m;		// Edges
 	int a,b,c;
-    while (myfile >> a){
+    while (myfile >> a){   // populate the adjacency lists
         myfile >> b >> c;
         G[a][b]=c;
         G[b][a]=c;
@@ -32,20 +35,25 @@ int main(int argc, char *argv[]){
     vector<int> spath;
     int sval=0;
 
-    spath = dijkstra(G,src,des);
+    spath = dijkstra(G,src,des);  // Run dijkstra to find shortest path
 
-    for(int i=0;i<spath.size()-1;i++){
+    // traverse through shortest path to find path weight
+    for(int i=0;i<spath.size()-1;i++){ 
     	int s=spath[i];
     	int d=spath[i+1];
     	sval += G[s][d];
     }
 
-    cout << sval << endl;
+    cout << sval << endl;   // Print the path weight
 
-    for(int i=0;i<spath.size();i++){
-    	cout << spath[i] << " ";
+    for(int i=0;i<spath.size();i++){ // print the path
+    	cout << spath[i] << " "; 
     }
     cout << endl;
+
+    // clock_t end = clock();
+    // double msecs = ((double) (end - start)) * 1000 / CLOCKS_PER_SEC;
+    // cout << "Time taken to run spp = " << msecs << " milliseconds" << endl;
 
 	return 0;
 }
